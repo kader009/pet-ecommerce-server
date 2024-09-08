3bc9c5ace644a48bc012b6352180f56e4221d295
kader009 <kadermollarasel000@gmail.com>
2024-09-08 10:53:05 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
-import express from "express";
-import cors from 'cors'
-import dotenv from 'dotenv'
-const port = process.env.PORT || 5000
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+const port = process.env.PORT || 5000;
 const app = express();
-dotenv.config()
+dotenv.config();
 
 // middleware
-app.use(express())
-app.use(cors())
+app.use(express());
+app.use(cors());
 
-app.get('/', (req, res) =>{
-  res.send('pet ecommerce site is active')
-})
+const uri = process.env.DB_URL;
 
-app.listen(port,() =>{
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('pet ecommerce site is active');
+});
+
+app.listen(port, () => {
   console.log(`pet ecommerce is on ${port}`);
-})
+});
