101868331eb21bde37c121c7a1dd58417aaf41e2
kader009 <kadermollarasel000@gmail.com>
2024-09-11 21:19:28 +0600

    update route create

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import { MongoClient, ObjectId, ServerApiVersion } from 'mongodb';
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -8,7 +8,12 @@ dotenv.config();
 
 // middleware
 app.use(express());
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    optionsSuccessStatus: 200,
+  })
+);
 
 const uri = process.env.DB_URL;
 
@@ -26,9 +31,41 @@ async function run() {
 
     const database = client.db('PetHouse');
     const petCollection = database.collection('petList');
+    const orderCollection = database.collection('order');
+    const userCollection = database.collection('user');
 
+    // get all data route
     app.get('/petlist', async (req, res) => {
       const result = await petCollection.find().toArray();
+      res.send(result);
+    });
+
+    // get single id route
+    app.get('/petlist/:id', async (req, res) => {
+      const id = req.params.id;
+      const result = await petCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    // update route for single pet
+    app.patch('/petlist/:id', async (req, res) => {
+      const id = req.params.id;
+      const update = req.body;
+
+      const result = await petCollection.updateOne(
+        {
+          _id: new ObjectId(id),
+        },
+        { $set: update }
+      );
+      res.send(result);
+    });
+
+    // post order
+    app.post('/order', async (req, res) => {
+      const order = req.body;
+      console.log('order:', order);
+      const result = await orderCollection.insertOne(order);
       res.send(result);
     });
 
